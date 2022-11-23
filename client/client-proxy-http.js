0fa4b90009f19615aff8ecc0d8fd4d5c65b302c0
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2022-11-23 23:27:18 +0900

    [update] convert to async function

--- client/client-proxy-http.js
+++ client/client-proxy-http.js
@@ -9,22 +9,40 @@ const http = require('http');
 const TARGET_URL = process.argv[2];
 const PROXY_URL = process.argv[3];
 
-const PARSE_TARGET_URL = new URL(TARGET_URL);
-const PARSE_PROXY_URL = PROXY_URL ?
-	new URL(PROXY_URL) : PARSE_TARGET_URL;
+main().catch(console.error);
 
-http.request({
-	hostname: PARSE_PROXY_URL.hostname,
-	port: PARSE_PROXY_URL.port,
-	path: PROXY_URL ? TARGET_URL : PARSE_TARGET_URL.pathname,
-	method: 'GET',
-	headers: {
-		Host: PARSE_TARGET_URL.host,
-	},
-}, res => {
-	for (let i = 0; i < res.rawHeaders.length; i += 2)
-		console.log(res.rawHeaders[i] + ': ' + res.rawHeaders[i + 1]);
-	console.log();
-	res.pipe(process.stdout);
-})
-.end();
+async function main() {
+	await httpRequest(process.stdout, TARGET_URL, PROXY_URL);
+}
+
+function httpRequest(writeStream, TARGET_URL, PROXY_URL) {
+
+	return new Promise((resolve, reject) => {
+
+		const PARSE_TARGET_URL = new URL(TARGET_URL);
+		const PARSE_PROXY_URL = PROXY_URL ?
+			new URL(PROXY_URL) : PARSE_TARGET_URL;
+
+		const CRLF = '\r\n';
+
+		const req = http.request({
+			hostname: PARSE_PROXY_URL.hostname,
+			port: PARSE_PROXY_URL.port,
+			path: PROXY_URL ? TARGET_URL : PARSE_TARGET_URL.pathname,
+			method: 'GET',
+			headers: {
+				Host: PARSE_TARGET_URL.host,
+			},
+		}, res => {
+			for (let i = 0; i < res.rawHeaders.length; i += 2)
+				writeStream.write(res.rawHeaders[i] + ': ' + res.rawHeaders[i + 1] + CRLF);
+			writeStream.write(CRLF);
+			res.pipe(writeStream, { end: false });
+			res.on('end', resolve);
+		});
+		req.on('error', reject);
+		req.end();
+
+	});
+
+}
